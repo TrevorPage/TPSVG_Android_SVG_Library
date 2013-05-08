105c561a284382675d4b8378541aaf3fb43ede36
Trevor Page <trevg60@gmail.com>
2013-05-08 21:29:58 +0100

    Two developments to SVGDrawable: (1) It now uses a bitmap to cache the rendered SVG image. Aside from the obvious performance benefit, doing this also cured a problem where the SVGDrawable looked aliased when used to provide icons in ActionBarSherlock. (2) An alternative constructor allows the Drawable's intrinsic dimensions to be specified, overriding those that would normally be taken from the SVG document. This is useful for example when using with ActionBarSherlock which calls setBounds() based on the intrinsic width and height; it means you can specify the icon size rather than it being too large due to the original SVG image size.

--- TPSVG/src/com/trevorpage/tpsvg/SVGDrawable.java
+++ TPSVG/src/com/trevorpage/tpsvg/SVGDrawable.java
@@ -1,45 +1,81 @@
 package com.trevorpage.tpsvg;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 
 public class SVGDrawable extends Drawable {
-	public SVGParserRenderer mRenderer;
+	private SVGParserRenderer mRenderer;
+	private final float mIntrinsicHeight;
+	private final float mIntrinsicWidth;
+	private Bitmap mCacheBitmap = null;
 
+	/**
+	 * Create a new SVGDrawable using the supplied renderer. This drawable's intrinsic width
+	 * and height will be the width and height specified in the SVG image document. 
+	 * @param renderer
+	 */
 	public SVGDrawable(SVGParserRenderer renderer) {
 		mRenderer = renderer;
+		mIntrinsicWidth = mRenderer.getDocumentWidth();
+		mIntrinsicHeight = mRenderer.getDocumentHeight();
 	}
 
+	/**
+	 * Create a new SVGDrawable using the supplied renderer. This drawable's intrinsic width
+	 * and height will be according to the values supplied, rather than taken from the 
+	 * SVG image document.  
+	 * @param renderer
+	 * @param intrinsicWidth
+	 * @param intrinsicHeight
+	 */
+	public SVGDrawable(SVGParserRenderer renderer, int intrinsicWidth, int intrinsicHeight) {
+		mRenderer = renderer;
+		mIntrinsicWidth = intrinsicWidth;
+		mIntrinsicHeight = intrinsicHeight;
+	}
+	
 	@Override
 	public void draw(Canvas canvas) {
 		Rect bounds = getBounds();
-
 		int height = bounds.height();
 		int width = bounds.width();
 
-		float documentHeight = mRenderer.getDocumentHeight();
-		float documentWidth = mRenderer.getDocumentWidth();
-
-		float scaleX = width / documentWidth;
-		float scaleY = height / documentHeight;
-
-		canvas.save();
-		canvas.scale(scaleX, scaleY);
-		mRenderer.paintImage(canvas, null, 0, 0, 0, null, false);
-		canvas.restore();
-
+		float scaleX = (float) width / mRenderer.getDocumentWidth();
+		float scaleY = (float) height / mRenderer.getDocumentHeight();
+		
+		if (mCacheBitmap == null) {
+			mCacheBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+			Canvas cacheCanvas = new Canvas(mCacheBitmap);
+			cacheCanvas.scale(scaleX, scaleY);
+			mRenderer.paintImage(cacheCanvas, null, 0, 0, 0, null, false);
+		}
+			
+		canvas.drawBitmap(mCacheBitmap, 0, 0, null);		
 	}
 
 	@Override
+	public void setBounds (int left, int top, int right, int bottom) {
+		super.setBounds(left, top, right, bottom);
+		mCacheBitmap = null;
+	}
+	
+	@Override
+	public void setBounds (Rect bounds) {
+		super.setBounds(bounds);
+		mCacheBitmap = null;
+	}
+	
+	@Override
 	public int getIntrinsicHeight() {
-		return mRenderer.getDocumentHeight();
+		return (int)mIntrinsicHeight;
 	}
 
 	@Override
 	public int getIntrinsicWidth() {
-		return mRenderer.getDocumentWidth();
+		return (int)mIntrinsicWidth;
 	}
 
 	@Override
